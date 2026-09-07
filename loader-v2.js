44f826764486f1b37a720e1d96a0f84d5a495325
intod4-source <int.od4@dawateislami.net>
2026-09-07 16:40:18 +0500

    Load Arabic category navigation update

--- loader-v2.js
+++ loader-v2.js
@@ -23,7 +23,7 @@
         recovery.src='./auth-recovery.js?v=20260901reset2';
         recovery.onload=()=>{
           const culture=document.createElement('script');
-          culture.src='./culture-v1.js?v=20260907arabic1';
+          culture.src='./culture-v1.js?v=20260907arabic2';
           culture.onload=()=>{
             const operations=document.createElement('script');
             operations.src='./language-operations.js?v=20260907ops2';
